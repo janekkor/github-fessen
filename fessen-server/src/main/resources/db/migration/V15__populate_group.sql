823ebebdb62d5eefd1603f9de23d598a57e4aa5a
Jan <janekk@hotmail.com>
2019-03-13 06:04:43 +0100

    Group-Tabelle hinzugefügt zur DB

---
+++ fessen-server/src/main/resources/db/migration/V15__populate_group.sql
@@ -0,0 +1,1 @@
+INSERT INTO APP_GROUP (ID, NAME, DESCRIPTION, MOD_USER, MOD_TIME) VALUES (1, 'Korolko', 'Familie Korolko beim Freitagsessen', 'admin', parsedatetime('2019-03-11 00:00:00.000', 'yyyy-MM-dd hh:mm:ss.SS'));
